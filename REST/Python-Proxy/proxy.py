cbd169cfcf0715ecb93faf2a58a0f9d7b30d9f04
tony-xlh <tony@dynamsoft.com>
2025-05-29 13:28:43 +0800

    remove the index route

--- REST/Python-Proxy/proxy.py
+++ REST/Python-Proxy/proxy.py
@@ -3,24 +3,6 @@ import requests
 app = Flask(__name__, static_url_path='/app', static_folder='./')
 SITE_NAME = "http://127.0.0.1:18622/"
 
-@app.route("/",methods=['GET','POST'])
-def index():
-    global SITE_NAME
-    if request.method=="GET":
-        resp = requests.get(f"{SITE_NAME}")
-        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-        headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-    elif request.method=="POST":
-        resp = requests.post(f"{SITE_NAME}",data=request.form)
-        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-
-
-
 @app.route("/<path:path>",methods=["GET","POST"])
 def proxy(path):
     global SITE_NAME
